69fa5941002e324fff0df20899d79139941c3e29
Osasuyi Osawe <oajo8732@gmail.com>
2023-01-10 10:55:49 -0800

    Refactor code.
    game has been completed

--- app.js
+++ app.js
@@ -2,12 +2,7 @@
 const gameNumber = Math.floor(Math.random() * 21);
 
 let score = 20;
-const gameReset = document
-  .querySelector(".reset")
-  .addEventListener("click", function () {
-    score = 0;
-    guess = 0;
-  });
+
 document.querySelector(".value").innerHTML = score;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -15,9 +10,11 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!guess) {
     document.querySelector(".message").textContent = "😒 NO NUMBER BUD";
     //When guess is too high
-  } else if (guess > gameNumber) {
+  }
+  if (guess !== gameNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "🥲 Too high Bud";
+      document.querySelector(".message").textContent =
+        guess > gameNumber ? "🥲 Too high Bud" : "😤 TOO LOW BUD";
       score--;
       document.querySelector(".value").textContent = score;
     } else {
@@ -27,24 +24,21 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector("body").style.backgroundColor = "red";
       document.querySelector("#result").textContent = gameNumber;
     }
-    //When guess is too low
-  } else if (guess < gameNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "😤 TOO LOW BUD";
-      score--;
-      document.querySelector(".value").textContent = score;
-    } else {
-      document.querySelector(".message").textContent =
-        "😑You Lost the Game. TRY AGAIN!";
-      document.querySelector(".value").textContent = 0;
-      document.querySelector("body").style.backgroundColor = "red";
-      document.querySelector("#result").textContent = gameNumber;
-    }
-    //When guess is equal to mystery number
-  } else if (guess === gameNumber) {
+  } else {
     document.querySelector(".message").textContent = "😊 YOU'RE A WEINER";
     document.querySelector("body").style.backgroundColor = "green";
     document.querySelector("#result").textContent = gameNumber;
-    document.querySelector("highScore").textContent = score;
+    document.querySelector("#highScore").textContent = score;
   }
+});
+
+let gameReset = document.querySelector(".reset");
+gameReset.addEventListener("click", function () {
+  console.log("You just clicked me!");
+
+  score = 20;
+  document.querySelector(".value").textContent = score;
+
+  document.querySelector("body").style.backgroundColor = "rgb(143, 143, 143)";
+  document.querySelector(".message").textContent = "Start Guessing...";
 });
